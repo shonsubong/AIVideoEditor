500ac9d90bb61bbd6ff75e6a6c274f68c756f942
shonsubong <starfox1211@naver.com>
2018-09-30 15:01:03 +0900

    최초 xamarin project 생성함

---
+++ AIVideoEditor/AIVideoEditor/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AIVideoEditor.Models;
+
+namespace AIVideoEditor.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
