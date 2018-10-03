78b020eb20921b70053885b7728a5e8b9d5b1a7c
shonsubong <starfox1211@naver.com>
2018-10-04 02:23:55 +0900

    Accord Library 추가 기본 Layout 개발 및 View 추가      VideoEditorPage      VideoClipLibraryView      VideoPreViewPlayView      VideoStoryBoardView

---
+++ AIVideoEditor/AIVideoEditor/ViewModels/VideoEditorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace AIVideoEditor.ViewModels
+{
+    public class VideoEditorViewModel : BaseViewModel
+    {
+        public VideoEditorViewModel()
+        {
+            Title = "인공지능 비디오 편집기";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ AIVideoEditor/AIVideoEditor/Views/VideoStoryBoardView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AIVideoEditor.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VideoStoryBoardView : ContentView
+	{
+		public VideoStoryBoardView ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
